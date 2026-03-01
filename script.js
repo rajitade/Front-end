90b4c3a88af5d42793298046afd908bffa2bdfaa
rajitade <rajitadesse12@gmail.com>
2026-03-01 15:16:18 +0300

    Version three

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// Version Two Script
-const btn = document.getElementById('changeBtn');
+// Version Three Script
 const text = document.getElementById('text');
+const changeBtn = document.getElementById('changeBtn');
+const resetBtn = document.getElementById('resetBtn');
 
-btn.addEventListener('click', () => {
+changeBtn.addEventListener('click', () => {
     text.textContent = "You clicked the button!";
     text.style.color = "blue";
+});
+
+resetBtn.addEventListener('click', () => {
+    text.textContent = "Hello World!";
+    text.style.color = "black";
 });
