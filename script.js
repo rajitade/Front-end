2d129b2445bf9ad8ee3052845eae335900642992
rajitade <rajitadesse12@gmail.com>
2026-03-01 15:09:51 +0300

    Version two

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// Version One Script
+// Version Two Script
 const btn = document.getElementById('changeBtn');
 const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
     text.textContent = "You clicked the button!";
+    text.style.color = "blue";
 });
